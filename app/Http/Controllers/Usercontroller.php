e65a6c5ffc7ec653f5c7415a1327e8967fc58397
sindhueee20 <105348571+sindhueee20@users.noreply.github.com>
2022-11-13 15:41:26 +0530

    Add files via upload

---
+++ app/Http/Controllers/Usercontroller.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class Usercontroller extends Controller
+{
+     function login (Request $request)
+    {
+        $user= User::where(['email'=>$request->email])->first();
+       // return $user->password;
+        if(! $user|| !Hash :: check($request->password, $user->password))
+        {
+            return "username or password is not matched";
+        }
+        else{
+            $request->session()->put('user', $user);
+            return redirect('/');
+        }
+    }
+
+    function register(Request $request)
+    {
+        // return $request->input();
+        $user= new User;
+        $user->name=$request->name;
+        $user->email=$request->email;
+        $user->password=Hash::make($request->password);
+        $user->save();
+        return redirect('/login');
+
+
+    }
+}
